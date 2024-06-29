8213b497f6a46c72095d658670812bb4eca18cdd
muawwaznc <muawwaznc@gmail.com>
2024-06-29 09:47:14 +0500

    adding all content

--- main.js
+++ main.js
@@ -4,47 +4,63 @@ const menuLinks = document.querySelectorAll('.menu-links li')
 const btnWhatsapp = document.querySelectorAll('button.whatsapp');
 
 function changeHeaderColor() {
-  const nav = document.querySelector('#navigation')
-  if (scrollY > 0) nav.classList.add('scroll')
-  else nav.classList.remove('scroll')
+    const nav = document.querySelector('#navigation')
+    const navbarLogo = document.querySelector('#nav-logo')
+    const logoDot = document.querySelector('#logo-dot')
+    if (scrollY > 0) 
+    {
+        nav.classList.add('scroll');
+        navbarLogo.classList.add('scroll');
+
+        logoDot.classList.add('yellow-logo-dot');
+        logoDot.classList.remove('black-logo-dot');
+    }
+    else 
+    {
+        nav.classList.remove('scroll');
+        navbarLogo.classList.remove('scroll');
+
+        logoDot.classList.add('black-logo-dot');
+        logoDot.classList.remove('yellow-logo-dot');
+    }
 }
 
 function showTopButton() {
-  const topBtn = document.querySelector('#back-top-button')
-  if (scrollY > 400) topBtn.classList.add('show')
-  else topBtn.classList.remove('show')
+    const topBtn = document.querySelector('#back-top-button')
+    if (scrollY > 400) topBtn.classList.add('show')
+    else topBtn.classList.remove('show')
 }
 
 function highlightMenu() {
-  const menus = document.querySelectorAll('.menu-links li a')
-  if (scrollY > 0 && scrollY < 700) menus[0].classList.add('active')
-  else menus[0].classList.remove('active')
+    const menus = document.querySelectorAll('.menu-links li a')
+    if (scrollY > 0 && scrollY < 700) menus[0].classList.add('active')
+    else menus[0].classList.remove('active')
 
-  if (scrollY >= 700 && scrollY < 1690) menus[1].classList.add('active')
-  else menus[1].classList.remove('active')
+    if (scrollY >= 700 && scrollY < 1690) menus[1].classList.add('active')
+    else menus[1].classList.remove('active')
 
-  if (scrollY >= 1690 && scrollY < 2415) menus[2].classList.add('active')
-  else menus[2].classList.remove('active')
+    if (scrollY >= 1690 && scrollY < 2415) menus[2].classList.add('active')
+    else menus[2].classList.remove('active')
 }
 
 function onScroll() {
-  changeHeaderColor()
-  showTopButton()
-  highlightMenu()
+    changeHeaderColor()
+    showTopButton()
+    highlightMenu()
 }
 
 function openMenu() {
-  const body = document.querySelector('body')
-  body.classList.add('menu-expanded')
+    const body = document.querySelector('body')
+    body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  const body = document.querySelector('body')
-  body.classList.remove('menu-expanded')
+    const body = document.querySelector('body')
+    body.classList.remove('menu-expanded')
 }
 
 function toWhatsapp() {
-  window.open('https://wa.me/5511937004490', '_blank');
+    window.open('https://wa.me/5511937004490', '_blank');
 }
 
 document.addEventListener('scroll', onScroll)
@@ -54,9 +70,9 @@ menuLinks.forEach(element => element.addEventListener('click', closeMenu))
 btnWhatsapp.forEach(element => element.addEventListener('click', toWhatsapp))
 
 ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
 }).reveal(`
   #home,
   #home img,
